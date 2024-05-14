73428b6cf982ac91e3d8dd1bbf3d5017ba9a990e
Abdul Zakir <abdulzakir632@gmail.com>
2024-05-14 15:41:42 +0500

    email integrated

--- src/components/ContactME.js
+++ src/components/ContactME.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import facebook from "../images/icons/facbook.png"
 import picon from "../images/icons/p-icon.png"
 import twitter from "../images/icons/twitter.png"
 import instagram from "../images/icons/instagram.png"
 import likedin from "../images/icons/linkedin.png"
 import youtube from "../images/icons/youtube.png"
+import emailjs from '@emailjs/browser';
 
-
+const myServiceKey="service_zavgx9p";
+const myTemplateId="template_5k0dpje";
+const my_Public_key="cXWuldV3_V-0J5vsl";
 function ContactME() {
+    const [alert, setAlert] = useState("");
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(`${myServiceKey}`, `${myTemplateId}`, form.current, {
+        publicKey: `${my_Public_key}`,
+      })
+      .then(
+        (e) => {
+            setAlert("Message Sent SuccessFully !",e.status)
+            setTimeout(() => {
+                setAlert("");
+            }, 200);
+          console.log('SUCCESS!');
+        },
+        (error) => {
+            setAlert("Failed",error.text)
+          console.log('FAILED...', error.text);
+        },
+    );
+  };
   return (
     <section className="contact-me card-bg" id="contact">
         <div className="container pt-5">
@@ -21,32 +47,41 @@ function ContactME() {
                     </div>
                 </div>
             </div>
-            <div className="row py-5 align-items-center">
+            <div className="row py-5 justify-content-center">
+                {/* 
+                    
                 <div className="col-md-6">
                     <div className="content">
                         <ul className="list-unstyled d-flex flex-column gap-4">
-                            <li className="text-white"><span className="text-orange">Address_</span>23 5 80 Road,Benedict,ne,68316 united States</li>
+                            <li className="text-white"><span className="text-orange">Address_</span>Blue Area , Islamabad , Pakistan</li>
                             <li className="text-white"><span className="text-orange">Phone_</span>+923411718783</li>
                             <li className="text-white"><span className="text-orange">Email_</span>abdulzakir632@gmail.com</li>
                             <li className="text-white"><span className="text-orange">Website_</span>wwww.w.pk.com</li>
                         </ul>
                     </div>
                 </div>
+                */}
                 <div className="col-md-6">
-                    <form className="bg-transparent">
-                        <input type="email" className="form-control rounded-3 input-bg" id="exampleFormControlInput1" placeholder="name@example.com" />
-                        <div className="my-4">
-                            <textarea className="form-control rounded-3 input-bg" id="exampleFormControlTextarea2" rows="8" placeholder="Message"></textarea>
+                    <form className="bg-transparent" ref={form} onSubmit={sendEmail}>
+                        {alert && <div class="alert alert-primary" role="alert">{alert}</div>}
+                        <div className="mb-2">
+                            <input type="text" className="form-control rounded-3 input-bg" name="user_name" placeholder="Name" />
                         </div>
-                        <button type="button" className="btn btncust py-3">Contact us</button>
+                        <div className=" mb-2">
+                            <input type="email" className="form-control rounded-3 input-bg" name="user_email" placeholder="name@example.com" />
+                        </div>
+                        <div className="my-4">
+                            <textarea className="form-control rounded-3 input-bg" name="message" rows="8" placeholder="Message"></textarea>
+                        </div>
+                        <button type="submit" className="btn btncust py-3 d-block w-100">Send Message</button>
                     </form>
                 </div>
             </div>
-            <div className="row g-3 py-3">
+            <div className="row g-3 py-3 mt-2">
                 <div className="col-12 d-flex justify-content-center">
                     <div className="w-75 text-center">
                         <h1 className="text-orange">AboutMe.</h1>
-                        <p className="text-white description">                I'm a passionate front-end web developer dedicated to crafting exceptional digital experiences. With a keen eye for design and a knack for problem-solving, I thrive on turning ideas into reality. Let's collaborate and create something extraordinary together.
+                        <p className="text-white description">I'm a passionate front-end web developer dedicated to crafting exceptional digital experiences. With a keen eye for design and a knack for problem-solving, I thrive on turning ideas into reality. Let's collaborate and create something extraordinary together.
                         </p>
                     </div>
                 </div>
